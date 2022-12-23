7b9cf86ffa9445cbcf68bddea98118bc9329b129
Hirono-Yukina <117709892+Hirono-Yukina@users.noreply.github.com>
2022-12-23 15:44:31 +0900

    練習問題２
    練習問題２の提出

--- 3-3/2.php
+++ 3-3/2.php
@@ -7,26 +7,15 @@ $rand_num = $key[$rand];
 <p> <?php echo date("Y/m/d"); ?>の運勢は</p>
 <p>選ばれた数字は<?php echo $rand_num; ?></p>
 <?php
-$rand_num = 0;
 if($rand_num == 0){
     echo "凶";
-}elseif($rand_num == 1){
+}elseif($rand_num <= 3){
     echo"小吉";
-}elseif($rand_num == 2){
-    echo"小吉";
-}elseif($rand_num == 3){
-    echo"小吉";
-}elseif($rand_num == 4){
+}elseif($rand_num <= 6){
     echo"中吉";
-}elseif($rand_num == 5){
-    echo"中吉";
-}elseif($rand_num == 6){
-    echo"中吉";
-}elseif($rand_num == 7){
+}elseif($rand_num <= 8){
     echo"吉";
-}elseif($rand_num == 8){
-    echo"吉";
-}elseif($rand_num == 9){
+}elseif($rand_num <= 9){
     echo"大吉";
 }
 ?>
