d9bb8308c7ba00e21485c3a291e0d47cf5a95b19
Hirono-Yukina <117709892+Hirono-Yukina@users.noreply.github.com>
2022-12-23 16:22:07 +0900

    ３章チェックテスト
    ３章チェックテストの提出

--- 3-4/answer.php
+++ 3-4/answer.php
@@ -1,38 +1,45 @@
+<link rel="stylesheet" href=css/answer.css>
+<body style = "background-color:gray;">
+
+
 <?php 
 $name = $_POST['name'];
-$number = $_POST['number'];
-$language = $_POST['language'];
-$command = $_POST['command'];
+$value = $_POST['number'];
+$value1 = $_POST['language'];
+$value2 = $_POST['command'];
 ?>
 
+<div class = "answer">
+    <p> <?php echo $name; ?> さんの結果は・・・？</p>
+    <div class = "value">
+        <p>①の答え</p>
+        <?php
+        if($value == 80){
+            echo "正解！";
+        }else{
+            echo "残念・・・"; 
+        }?>
+    </div>
 
-<p> <?php echo $name; ?> さんの結果は・・・？</p>
-<p>①の答え</p>
-<?php
-$number = 80;
-if($number == 80){
-    echo "正解！";
-}else{
-    echo "残念・・・";
-}
-?>
+    <div class = "value1">
+        <p>②の答え</p> 
+        <?php
+        if($value1 == "HTML"){
+            echo "正解！";
+        }else{
+            echo "残念・・・";
+        }
+        ?>
+    </div>
+    
+    <div class = "value2">
+        <p>③の答え</p> 
+        <?php
+        if($value2 == "select"){
+            echo "正解！";
+        }else{
+            echo "残念・・・";
+        }?>
+    </div>
+</div>
 
-<p>②の答え</p>
-<?php
-$language = "HTML";
-if($language == "HTML"){
-    echo "正解！";
-}else{
-    echo "残念・・・";
-}
-?>
-
-<p>③の答え</p>
-<?php
-$command = "select" ;
-if($command == "select"){
-    echo "正解！";
-}else{
-    echo "残念・・・";
-}
-?>
--- 3-4/question.php
+++ 3-4/question.php
@@ -7,44 +7,39 @@ $my_name = $_POST['my_name'];
 
 <p>お疲れ様です <?php echo $my_name; ?>さん</p>
 
-<h2>①ネットワークのポート番号は何番？</h2>
-<div class = "number">
-<?php 
-$number=["80","22","20","21"];
-foreach($number as $value){  ?>
-    <input type="radio" name ="number" value = $number>
-    <?php echo $value; 
-}
-?>
-
-</div>
-
-<h2>②Webページを作成するための言語は？</h2>
-<div class = "language">
-<?php 
-$language=["PHP","Python","JAVA","HTML"];
-foreach($language as $value){ ?> 
-    <input type="radio" name ="langage" value = $language>
-    <?php echo $value; 
-}
-?>
-</div>
-
-
-<h2>③MySQLで情報を取得するためのコマンドは？</h2>
-<div class = "command">
-<?php 
-$command=["join","select","insert","update"];
-foreach($command as $value){ ?> 
-    <input  type="radio" name ="command" value = $command>
-    <?php echo $value; 
-}
-?>
-</div>
-
         
 <div class="from">
     <form action="answer.php" method="post">
+        <h2>①ネットワークのポート番号は何番？</h2>
+        <div class = "number">
+            <?php
+            $number=["80","22","20","21"];
+            foreach($number as $value){  
+                echo "<input type='radio' name ='number' value = '${value}'> ${value}"; 
+            } 
+            ?>
+        </div>
+
+        <div class = "language">
+            <h2>②Webページを作成するための言語は？</h2>
+            <?php 
+            $language=["PHP","Python","JAVA","HTML"];
+            foreach($language as $value1){ 
+                echo "<input type='radio' name ='language' value = '${value1}'> ${value1}"; 
+            }
+            ?>
+        </div>
+
+        <div class = "command">
+            <h2>③MySQLで情報を取得するためのコマンドは？</h2>
+            <?php
+            $command=["join","select","insert","update"];
+            foreach($command as $value2){ 
+                echo "<input  type='radio' name ='command' value = '${value2}'> ${value2}"; 
+            }
+            ?>
+        </div>
+        <br>
         <input type="submit" style="font-size: 8px;" value="回答する" class="submit"/>
         <input type ="hidden" name ="name" value="<?php echo $my_name; ?> ">
     </form>
